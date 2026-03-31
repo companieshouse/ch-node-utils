430253d8afa8c77487789f1af8244874e73738d9
eedwards0 <eedwards2@companieshouse.gov.uk>
2026-03-31 16:20:49 +0100

    Fix leading underscore ESLint error

--- src/utils/languageNames.ts
+++ src/utils/languageNames.ts
@@ -26,7 +26,7 @@ export default class LanguageNames {
     }
 
     // custom sort to leave "en" always at 1st position
-    private static _customSort(a: string, b: string): number {
+    private static customSort(a: string, b: string): number {
         if (a === "en") { return -1; } // "en" is considered smaller, so it will be placed at the beginning
         if (b === "en") { return 1; }
 
@@ -35,7 +35,7 @@ export default class LanguageNames {
 
     static sourceLocales( localesDir: string ): NamedIsoCode[] {
         const localesArray = SubDirs.getSubDirs (localesDir);
-        return LanguageNames.getNativeNamesObjectArray (localesArray.sort(LanguageNames._customSort));
+        return LanguageNames.getNativeNamesObjectArray (localesArray.sort(LanguageNames.customSort));
     }
 
     static isSupportedLocale( localesDir: string, locale: string ): boolean {
