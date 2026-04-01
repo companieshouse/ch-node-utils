c3a97f441ddc7bf164fa1300a75e6fbd91a3e337
eedwards0 <eedwards2@companieshouse.gov.uk>
2026-04-01 10:56:34 +0100

    Remove lib contents and revert .gitignore

--- lib/services/locales/locales.service.d.ts
+++
@@ -1,9 +0,0 @@
-import i18nCh from "../../utils/i18nCh";
-export default class LocalesService {
-    private static instance;
-    enabled: boolean;
-    localesFolder: string;
-    i18nCh: i18nCh;
-    private constructor();
-    static getInstance(localesFolder?: string, enabled?: boolean): LocalesService;
-}
--- lib/utils/sessionUtils.d.ts
+++
@@ -1,2 +0,0 @@
-import { Session } from "@companieshouse/node-session-handler";
-export declare function isAuthorisedAgent(session: Session | undefined): boolean;
