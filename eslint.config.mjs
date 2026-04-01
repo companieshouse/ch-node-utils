85f36a2914e960a76ae9f3b8b8e812f0683f47aa
eedwards0 <eedwards2@companieshouse.gov.uk>
2026-04-01 08:59:40 +0100

    Resolve problems with types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,12 @@ const configArray = [
             },
 
             parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: "module",
+                project: "./tsconfig.json",
+                tsconfigRootDir: __dirname,
+            },
             ecmaVersion: "latest",
             sourceType: "commonjs",
         },
